9340337889fde0f742e72c3699605183871d2477
vladyslav <vladiki2396@gmail.com>
2025-01-10 17:39:09 +0100

    Fix: Removed unused variable 'Footer' from Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
 import FaqSection from '../components/FaqSection';
-import Footer from '../components/Footer';
 import Divider from '../components/Divider';
 
 const Home = () => {
