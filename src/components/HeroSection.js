e74dc707d05e6e36363d485168a69d22bfb02dbd
inmenton <vladiki23@icloud.com>
2025-03-27 19:36:17 +0100

    fix: background video now fills entire screen

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,14 +4,23 @@ import './HeroSection.css';
 const HeroSection = () => {
     return (
         <div className="hero-container">
-            {/* Видео на фоне */}
-            <video autoPlay loop muted className="hero-video">
-                <source src="/video/background.mp4.mov" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            <div className="video-wrapper">
+                <iframe
+                    title="vimeo-player"
+                    src="https://player.vimeo.com/video/1070058633?h=8fdb35769f&autoplay=1&loop=1&muted=1&background=1"
+                    frameBorder="0"
+                    allow="autoplay; fullscreen; picture-in-picture"
+                    allowFullScreen
+                    className="hero-video"
+                ></iframe>
+            </div>
         </div>
     );
 };
 
 export default HeroSection;
+
+
+
+
 
