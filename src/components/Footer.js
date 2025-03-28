94b5f3c01fc32419987d236afc461975aa6b37af
inmenton <vladiki23@icloud.com>
2025-03-28 13:39:41 +0100

    Fix text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { FaEnvelope, FaWhatsapp } from 'react-icons/fa'; // Импортируем иконки
 
 const Footer = () => {
     return (
         <footer className="bg-dark text-white py-4">
-            <div className="container d-flex justify-content-between align-items-center">
+            <div className="container d-flex flex-column flex-md-row justify-content-between align-items-center">
                 {/* Копирайт слева */}
                 <div className="footer-copyright">
                     <p className="mb-0">&copy; 2024 drone-pilot.co.uk. All Rights Reserved.</p>
                 </div>
 
                 {/* Контакты справа */}
-                <div className="footer-contact text-end">
+                <div className="footer-contact text-center text-md-end">
                     <h5>Contact Me</h5>
-                    <p className="mb-1">Email: pro.dronepilot.uk@gmail.com</p>
-                    <p>WhatsApp: +4474 939 12 469</p>
+                    <div className="footer-icons mb-2">
+                        <p className="mb-1">
+                            <FaEnvelope /> <span>pro.dronepilot.uk@gmail.com</span>
+                        </p>
+                        <p>
+                            <FaWhatsapp /> <span>+4474 939 12 469</span>
+                        </p>
+                    </div>
                 </div>
             </div>
         </footer>
@@ -21,6 +28,7 @@ const Footer = () => {
 };
 
 export default Footer;
+
 
 
 
