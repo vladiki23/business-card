8f4a79cd2f261d9c322d6f1d7984110bc2eea052
inmenton <vladiki23@icloud.com>
2025-03-28 10:51:51 +0100

    новое сообщение

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,21 +4,25 @@ import './HeroSection.css';
 const HeroSection = () => {
     return (
         <div className="hero-container">
-            <div className="video-wrapper">
+            <div className="video-container">
                 <iframe
-                    title="vimeo-player"
-                    src="https://player.vimeo.com/video/1070058633?h=8fdb35769f&autoplay=1&loop=1&background=1" // убрали muted=1
+                    src="https://player.vimeo.com/video/1070058633?h=8fdb35769f&autoplay=1&loop=1&muted=1&background=1"
+                    title="vimeo-background"
                     frameBorder="0"
                     allow="autoplay; fullscreen; picture-in-picture"
                     allowFullScreen
-                    className="hero-video"
-                ></iframe>
+                />
             </div>
         </div>
     );
 };
 
 export default HeroSection;
+
+
+
+
+
 
 
 
