c8949934e8aa7228394ab549ed0c41b44ce4b8aa
inmenton <vladiki23@icloud.com>
2025-03-29 01:19:52 +0100

    Fix text

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,7 +9,7 @@ const HeroSection = () => {
                     title="vimeo-background"
                     src="https://player.vimeo.com/video/1070058633?h=8fdb35769f&autoplay=1&loop=1&muted=1&background=1&title=0&byline=0&portrait=0"
                     frameBorder="0"
-                    allow="autoplay; fullscreen; picture-in-picture"
+                    allow="autoplay=0; fullscreen; picture-in-picture"
                     allowFullScreen
                     className="hero-video"
                 />
